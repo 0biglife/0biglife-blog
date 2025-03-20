cbc3589e61e1d0017cbbb6f5d3d064c00dae74eb
0biglife <0biglife@gmail.com>
2025-03-21 02:46:28 +0900

    feat: hard text to constants

---
+++ src/lib/constant.ts
@@ -0,0 +1,27 @@
+// TOOD : i18n
+
+// Blog MetaData
+export const BLOG_EMAIL = "0biglife@gmail.com";
+export const BLOG_URL = "https://0biglife.com";
+
+// Landing Page
+export const BLOG_LEFT_TOP_CATEGORY = "Recently Featured";
+export const BLOG_RIGHT_TOP_CATEGORY = "Dev Logs";
+export const CATEGORY_TITLE = "Category";
+export const TODAY_COUNT = "Today";
+export const TOTAL_COUNT = "Total";
+
+// Not Found
+export const NOT_FOUND_STATUS_CODE = "404";
+export const NOT_FOUND_MESSAGE = "페이지를 찾을 수 없습니다.";
+export const NOT_FOUND_HELP_FORWARD_TEXT = "혹시 문제가 발생하였다면";
+export const NOT_FOUND_HELP_BACKWARD_TEXT = "로 이메일로 요청해주세요.";
+export const HOME_BUTTON_TEXT = "홈으로 돌아가기";
+
+// Components
+export const EMPTY_TITLE = "No Title"; // 제거
+export const EMPTY_POST = "No Post";
+export const THUMNAIL_DATE_TEXT = "Updated: ";
+
+// etc
+export const TESTING = "테스트중";
